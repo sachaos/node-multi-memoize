f47690a1001df8d2585e75a6d66f63f860cf4254
Takumasa Sakao <sakataku7@gmail.com>
2020-07-12 20:45:36 +0900

    Fix bug of chain

--- src/maps/Chain.ts
+++ src/maps/Chain.ts
@@ -3,9 +3,9 @@ import {AsyncMapCache} from "../types";
 export class Chain implements AsyncMapCache {
     constructor(private maps: AsyncMapCache[]) {}
 
-    async get(key: string): Promise<{ value: any; ok: boolean }> {
+    async get(key: string, namespace: string): Promise<{ value: any; ok: boolean }> {
         for (let map of this.maps) {
-            const { value, ok } = await map.get(key)
+            const { value, ok } = await map.get(key, namespace)
 
             if (ok) return {value: value, ok: true}
         }
@@ -13,9 +13,9 @@ export class Chain implements AsyncMapCache {
         return {value: undefined, ok: false}
     }
 
-    async set(key: string, value: any): Promise<this> {
+    async set(key: string, value: any, namespace: string): Promise<this> {
         for (let map of this.maps.reverse()) {
-            await map.set(key, value)
+            await map.set(key, value, namespace)
         }
 
         return this
