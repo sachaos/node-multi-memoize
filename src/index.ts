0bea07aabded6ba986255afa58a50574c5880c1f
Mamoru Amano <booktower103@gmail.com>
2020-10-05 21:15:01 +0900

    Fix: use arguments in all situations

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import {AsyncMapCache, MemoizedFunction} from "./types";
 import {AsyncMap} from "./maps";
 
-export function mmemoize<S, T extends (args: any) => Promise<S>>(func: T, opts: { resolver?: (...args: any[]) => string, map?: AsyncMapCache, namespace?: string } = {}): T & MemoizedFunction {
-    const memoized = (async (args: any): Promise<S> => {
+export function mmemoize<S, T extends (...args: any[]) => Promise<S>>(func: T, opts: { resolver?: (...args: any[]) => string, map?: AsyncMapCache, namespace?: string } = {}): T & MemoizedFunction {
+    async function memoized(_: any): Promise<S> {
         const { resolver, namespace } = opts
-        const key = resolver ? resolver(args) : String(arguments[0])
+        const key = resolver ? resolver(...arguments) : String(arguments[0]);
 
         const cache = memoized.cache
 
@@ -13,15 +13,15 @@ export function mmemoize<S, T extends (args: any) => Promise<S>>(func: T, opts: 
             return value
         }
 
-        const result = await func(args)
+        const result = await func(...arguments);
         await cache.set(key, result, namespace)
 
         return result
-    }) as T & MemoizedFunction
+    }
 
     memoized.cache = opts?.map || new AsyncMap()
 
-    return memoized
+    return memoized as T & MemoizedFunction;
 }
 
 export default mmemoize
